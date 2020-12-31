9d18c7d8a211b6bb38198edc96693abea3668057
Shivam Potdar <shivampotdar99@gmail.com>
2020-12-31 17:04:34 +0530

    initial commit

---
+++ sim/ncompile/src_rtl_files.f
@@ -0,0 +1,22 @@
+/home/sumanth/shivam/pulp/pulpissimo/rtl/tb/riscv_pkg.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/tb/jtag_pkg.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/tb/pulp_tap_pkg.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/tb/tb_clk_gen.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/tb/tb_fs_handler.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/tb/dpi_models/dpi_models.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/tb/tb_driver/tb_driver.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/tb/tb_pulp.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/tb/SimJTAG.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/tb/SimDTM.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/vip/spi_master_padframe.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/vip/uart_tb_rx.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/vip/camera/cam_vip.sv
+-incdir /home/sumanth/shivam/pulp/pulpissimo/rtl/pulpissimo/../includes
+/home/sumanth/shivam/pulp/pulpissimo/rtl/pulpissimo/jtag_tap_top.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/pulpissimo/pad_control.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/pulpissimo/pad_frame.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/pulpissimo/safe_domain.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/pulpissimo/soc_domain.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/pulpissimo/rtc_date.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/pulpissimo/rtc_clock.sv
+/home/sumanth/shivam/pulp/pulpissimo/rtl/pulpissimo/pulpissimo.sv
